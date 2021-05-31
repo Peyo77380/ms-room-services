41561e3cce6185f2cf13dc7b26fea278e02283c9
GUICHARD Pierre <pierre.guichard77@gmail.com>
2021-05-31 15:31:00 +0200

    clean migration

--- database/migrations/2021_05_31_120900_create_buildings_table.php
+++ database/migrations/2021_05_31_120900_create_buildings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBuildingsCollection extends Migration
+class CreateBuildingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBuildingsCollection extends Migration
      */
     public function up()
     {
-        Schema::create('buildings_collection', function (Blueprint $table) {
+        Schema::create('buildings', function (Blueprint $table) {
             $table->id();
             $table->json('location');
             $table->int('surface');
@@ -35,6 +35,6 @@ class CreateBuildingsCollection extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('buildings_collection');
+        Schema::dropIfExists('buildings');
     }
 }
