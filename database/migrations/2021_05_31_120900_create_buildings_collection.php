be46ad081de5b16a4592455777964e62cc0fa8fd
GUICHARD Pierre <pierre.guichard77@gmail.com>
2021-05-31 14:24:22 +0200

    add buildings migration

---
+++ database/migrations/2021_05_31_120900_create_buildings_collection.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuildingsCollection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings_collection', function (Blueprint $table) {
+            $table->id();
+            $table->json('location');
+            $table->int('surface');
+            $table->json('openingHours');
+            $table->string('description');
+            $table->id('pictures'); // à remplacer par foreign key quand upload image est prêt
+            $table->json('characteristics');
+            $table->int('state');
+            $table->boolean('enabled');
+            $table->json('floor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings_collection');
+    }
+}
