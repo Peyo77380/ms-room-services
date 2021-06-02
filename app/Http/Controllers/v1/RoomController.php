4100ac5f82c659c87dbd210da1b026f1c6586967
GUICHARD Pierre <pierre.guichard77@gmail.com>
2021-06-02 14:38:09 +0200

    create room model

---
+++ app/Http/Controllers/v1/RoomController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Models\Room;
+
+use App\Traits\ApiResponder;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+
+class RoomController extends Controller
+{
+    use ApiResponder;
+
+    
+
+}
+
---
+++ app/Models/Room.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        // TODO : foreign key
+        'buildingId',
+        // TODO : foreign key
+        'floorId',
+        'surface',
+        'maxCapacity',
+        'rules',
+        'color',
+        // TODO : à demander au client : est-ce nécessaire de mettre un champ correspondant aux horaires d'ouvertures (si jamais une salle est ouverte moins longtemps que le batiment dans lequel elle se situe)
+        'openingHours',
+        'enabled',
+        'services',
+        'type'
+    ];
+
+}
